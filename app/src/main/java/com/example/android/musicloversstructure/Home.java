0909242e1d286402faf7c6976ff4f999470189f7
Carlos Lamb <thebiggcaok@gmail.com>
2017-05-01 20:49:10 +0200

    Refactor !

---
+++ app/src/main/java/com/example/android/musicloversstructure/Home.java
@@ -0,0 +1,16 @@
+package com.example.android.musicloversstructure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+
+    }
+}
