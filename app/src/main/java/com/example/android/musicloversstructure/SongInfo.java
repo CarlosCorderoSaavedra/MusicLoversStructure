2412dd2acfbc9a4454778810ada7a3b44605b284
Carlos Lamb <thebiggcaok@gmail.com>
2017-05-02 19:32:20 +0200

    Song Info Layout

--- app/src/main/java/com/example/android/musicloversstructure/SongInfo.java
+++ app/src/main/java/com/example/android/musicloversstructure/SongInfo.java
@@ -1,8 +1,23 @@
 package com.example.android.musicloversstructure;
 
-/**
- * Created by Shine on 02/05/2017.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class SongInfo {
+
+
+public class SongInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_info);
+    }
+
+    public void openSongInfo(View view) {
+        Intent songInfo = new Intent(this, SongInfo.class);
+        startActivity(songInfo);
+        finish();
+    }
 }
--- app/src/main/java/com/example/android/musicloversstructure/SongList.java
+++ app/src/main/java/com/example/android/musicloversstructure/SongList.java
@@ -2,6 +2,7 @@ package com.example.android.musicloversstructure;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,19 +14,19 @@ public class SongList extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
         // Create 50 dummy songs to populate the list view
-        String songName = getResources().getString(R.string.add_song_name);
         String songAuthor = getResources().getString(R.string.add_song_artist_name);
+        String songName = getResources().getString(R.string.add_song_name);
         int songsCount = 50;
         int i = 0;
-        ArrayList<String[]> songsList = new ArrayList<>();
+        ArrayList<String> songsList = new ArrayList<>();
         while( i < songsCount){
-            String[] song = new String[]{ songName, songAuthor};
-            songsList.add(i, song);
+            songsList.add(songAuthor + " " + " - " + " " + songName);
             i++;
         }
         // Set an adapter on the list view
-        ListView songList = (ListView) findViewById(R.id.adapter_songs_list);
-        songList.setAdapter(new SongsAdapter(this, songsList ));
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songsList);
+        ListView listView = (ListView) findViewById(R.id.song_list);
+        listView.setAdapter(itemsAdapter);
 
         }
 }
