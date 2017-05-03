0882009cb41dbecc14015d4b0708de8b86aba4c5
Carlos Lamb <thebiggcaok@gmail.com>
2017-05-03 23:17:55 +0200

    added player

---
+++ app/src/main/java/com/example/android/musicloversstructure/Player.java
@@ -0,0 +1,10 @@
+package com.example.android.musicloversstructure;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shine on 03/05/2017.
+ */
+
+public class Player extends AppCompatActivity {
+}
